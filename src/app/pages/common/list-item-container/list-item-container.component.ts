155dd49577dd01b70d62fbbdd02c370bd0e2ddee
IvanToth <ivan.tothba@gmail.com>
2024-11-19 17:39:27 +0100

    * Animation for list item added * minor layout adjustments

--- src/app/pages/common/list-item-container/list-item-container.component.ts
+++ src/app/pages/common/list-item-container/list-item-container.component.ts
@@ -1,6 +1,7 @@
 import { Component, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { CommonEntertainmentData } from '../../../shared/model/shared.model';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-list-item-container',
@@ -8,6 +9,17 @@ import { CommonEntertainmentData } from '../../../shared/model/shared.model';
   imports: [MatCardModule],
   templateUrl: './list-item-container.component.html',
   styleUrl: './list-item-container.component.scss',
+  animations: [
+    trigger('shake', [
+      transition(':enter', [
+        style({ transform: 'rotate(0)' }),
+        animate('0.1s', style({ transform: 'rotate(2deg)' })),
+        animate('0.1s', style({ transform: 'rotate(-2deg)' })),
+        animate('0.1s', style({ transform: 'rotate(2deg)' })),
+        animate('0.1s', style({ transform: 'rotate(0)' })),
+      ]),
+    ]),
+  ],
 })
 export class ListItemContainerComponent {
   item = input.required<CommonEntertainmentData>();
